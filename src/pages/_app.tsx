a1a5fee5d0f61e3b3553947770cf97da5db1494d
Natanael Lourenço <holasoycael@gmail.com>
2021-11-29 21:34:46 -0300

    Include Mercadopago.js and types at project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 import GlobalStyle from 'styles/global'
@@ -5,6 +6,7 @@ import GlobalStyle from 'styles/global'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js" />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
